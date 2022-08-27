989ec1b06fc7637b293675c5310d9dd8192d44d4
Manan1612 <mananpatel161200@gmail.com>
2022-08-27 13:46:04 -0400

    Add files via upload

---
+++ code/setup.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+n=int(input("Input a Number : "))
+print("factorial of {} is {}".format(n,factorial(n)))
+
+(f"factorial of {n} is {factorial(n)}")
