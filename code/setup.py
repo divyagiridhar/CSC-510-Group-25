e2cbe8f7ea233b68e4a921f4aaf0d8d3fc1aee94
Manan1612 <mananpatel161200@gmail.com>
2022-08-28 16:36:30 -0400

    Update setup.py

--- code/setup.py
+++ code/setup.py
@@ -1,7 +1,11 @@
+import sys
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
-n=int(input("Input a Number : "))
-print("factorial of {} is {}".format(n,factorial(n)))
+
+if _name_ == '_main_':
+    arg = sys.argv
+    n = int(arg[1])
+    print("factorial of {} is {}".format(n,factorial(n)))
