340543e657cf1074b0618df2c0734e95ef3968a8
Manan1612 <mananpatel161200@gmail.com>
2022-08-31 17:07:58 -0400

    Uploaded algorithm codes

---
+++ test/fibo.py
@@ -0,0 +1,11 @@
+import sys
+
+def fibonacci(n):
+	if n <= 1:
+		return n
+	return fibonacci(n-1) + fibonacci(n-2)
+
+if __name__ == "__main__":
+    arg = sys.argv
+    n = int(arg[1])
+    print("The {}th fibonacci number is {}".format(n,fibonacci(n))) 
